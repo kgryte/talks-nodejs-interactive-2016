2d9689f8d87c91383a7b27cb6027ec637acf22d2
kgryte <kgryte@gmail.com>
2016-11-27 21:22:57 -0800

    Rename script and add Math.cos example

--- scripts/bugs/math_sin_cos.js
+++ scripts/bugs/math_sin_cos.js
@@ -6,3 +6,9 @@ var expected = 0.377820109360752;
 
 // See also https://bugs.chromium.org/p/v8/issues/detail?id=3089.
 console.log( 'Math.sin(%d) = %d. Expected: %d.', x, actual, expected );
+
+actual = Math.cos( x );
+expected = -0.9258790228548378;
+
+// See also https://bugs.chromium.org/p/v8/issues/detail?id=3006.
+console.log( 'Math.cos(%d) = %d. Expected: %d.', x, actual, expected );
